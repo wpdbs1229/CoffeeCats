b719972cbd1d9f687203faafeaa1c87813b2e54b
LeeJeYoun <dlwpdbs1229@naver.com>
2025-01-08 16:17:16 +0900

    Feat :#9-카페-위치기반-조회-기능-구현(GoogleMapAPI) - Google:place:searchNearby 클라이언트 추가

---
+++ src/main/java/org/hidevelop/coffeecats/client/GooglePlaceClient.java
@@ -0,0 +1,25 @@
+package org.hidevelop.coffeecats.client;
+
+import feign.Headers;
+import feign.Param;
+import org.hidevelop.coffeecats.config.GooglePlaceFeignConfig;
+import org.hidevelop.coffeecats.model.dto.GooglePlaceRequestDto;
+import org.hidevelop.coffeecats.model.dto.GooglePlaceResponseDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@FeignClient(name = "google-place-client",
+        url = "https://places.googleapis.com/v1/",
+        configuration = GooglePlaceFeignConfig.class)
+public interface GooglePlaceClient {
+
+    @PostMapping(value = "places:searchNearby",
+            headers = {"Content-Type=application/json",
+                    "X-Goog-FieldMask=places.displayName,places.name,places.formattedAddress,places.location"}
+    )
+    GooglePlaceResponseDto searchNearby(
+            GooglePlaceRequestDto googlePlaceRequestDto
+    );
+}
