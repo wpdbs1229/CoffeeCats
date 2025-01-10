cbce935a11d4cc9045a0f51210d6b2337ad696c8
LeeJeYoun <dlwpdbs1229@naver.com>
2025-01-10 17:16:16 +0900

    feat :#11-구글-지도-장소-검색-api-구현 - GoogleMapSearchText 통신 클라이언트 구현

--- src/main/java/org/hidevelop/coffeecats/client/GooglePlaceClient.java
+++ src/main/java/org/hidevelop/coffeecats/client/GooglePlaceClient.java
@@ -1,11 +1,11 @@
 package org.hidevelop.coffeecats.client;
 
 import org.hidevelop.coffeecats.config.GooglePlaceFeignConfig;
-import org.hidevelop.coffeecats.model.dto.GooglePlaceSearchNearByRequestDto;
-import org.hidevelop.coffeecats.model.dto.GooglePlaceResponseDto;
+import org.hidevelop.coffeecats.model.dto.google_map.GooglePlaceSearchNearByRequestDto;
+import org.hidevelop.coffeecats.model.dto.google_map.GooglePlaceResponseDto;
+import org.hidevelop.coffeecats.model.dto.google_map.GooglePlaceSearchTextReqDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
-
 
 
 @FeignClient(name = "google-place-client",
@@ -19,5 +19,13 @@ public interface GooglePlaceClient {
     )
     GooglePlaceResponseDto searchNearby(
             GooglePlaceSearchNearByRequestDto googlePlaceSearchNearByRequestDto
+    );
+
+    @PostMapping(value = "places:searchText",
+        headers = {"Content-Type=application/json",
+                "X-Goog-FieldMask=places.displayName,places.name,places.formattedAddress,places.location"}
+    )
+    GooglePlaceResponseDto searchText(
+            GooglePlaceSearchTextReqDto googlePlaceSearchTextReqDto
     );
 }
