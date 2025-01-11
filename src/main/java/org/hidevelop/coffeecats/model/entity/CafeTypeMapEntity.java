e74efce7d22ab637bb100188c7f663fd5baec2d6
LeeJeYoun <dlwpdbs1229@naver.com>
2025-01-11 15:22:30 +0900

    refactor :#13-엔티티-멤버-변수-추가-및-신규-엔티티-생성 - CafeTypeMapEntity이 typeReviewCount 멤버 필드 추가

--- src/main/java/org/hidevelop/coffeecats/model/entity/CafeTypeMapEntity.java
+++ src/main/java/org/hidevelop/coffeecats/model/entity/CafeTypeMapEntity.java
@@ -2,6 +2,7 @@ package org.hidevelop.coffeecats.model.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.Comment;
 import org.hidevelop.coffeecats.model.entity.id.CafeTypeMapId;
 
 @IdClass(CafeTypeMapId.class)
@@ -21,9 +22,13 @@ public class CafeTypeMapEntity extends BaseEntity {
     @JoinColumn(name = "cafe_type_id", nullable = false)
     private CafeTypeEntity cafeType;
 
+    @Comment("해당 태그가 리뷰된 수")
+    private int typeReviewCount;
+
     public CafeTypeMapEntity(CafeEntity cafeEntity, CafeTypeEntity cafeTypeEntity) {
         this.cafe = cafeEntity;
         this.cafeType = cafeTypeEntity;
+        this.typeReviewCount = 1;
     }
 
 }
