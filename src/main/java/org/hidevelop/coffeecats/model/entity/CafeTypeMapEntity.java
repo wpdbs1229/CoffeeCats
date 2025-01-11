21a560997ae08fa10a914d4c6c5e64338a88a0ca
LeeJeYoun <dlwpdbs1229@naver.com>
2025-01-11 15:16:46 +0900

    refactor :#13-엔티티-멤버-변수-추가-및-신규-엔티티-생성 - CafeTypeMapEntity이 Id 멤버 필드를 cafe, cafeType 복합키로 변경

--- src/main/java/org/hidevelop/coffeecats/model/entity/CafeTypeMapEntity.java
+++ src/main/java/org/hidevelop/coffeecats/model/entity/CafeTypeMapEntity.java
@@ -2,29 +2,28 @@ package org.hidevelop.coffeecats.model.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hidevelop.coffeecats.model.entity.id.CafeTypeMapId;
 
+@IdClass(CafeTypeMapId.class)
 @Entity
 @Table(name = "cafe_type_map")
 @Getter
-@AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class CafeTypeMapEntity extends BaseEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
     @ManyToOne
     @JoinColumn(name = "cafe_id", nullable = false)
     private CafeEntity cafe;
 
+    @Id
     @ManyToOne
     @JoinColumn(name = "cafe_type_id", nullable = false)
     private CafeTypeEntity cafeType;
-
 
     public CafeTypeMapEntity(CafeEntity cafeEntity, CafeTypeEntity cafeTypeEntity) {
         this.cafe = cafeEntity;
         this.cafeType = cafeTypeEntity;
     }
+
 }
