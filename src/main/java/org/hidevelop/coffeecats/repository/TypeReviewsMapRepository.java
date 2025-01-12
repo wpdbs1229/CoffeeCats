f4a5b7592b5d3f2e32c62ad5a9989e7215c54ffb
LeeJeYoun <dlwpdbs1229@naver.com>
2025-01-12 15:35:14 +0900

    refactor : "카페 등록 기능 수정" - DB와 통신하기 위한 MemberCafeTypeReviewsRepository, TypeReviewsMapRepository 생성 Related to: #15

---
+++ src/main/java/org/hidevelop/coffeecats/repository/TypeReviewsMapRepository.java
@@ -0,0 +1,8 @@
+package org.hidevelop.coffeecats.repository;
+
+import org.hidevelop.coffeecats.model.entity.TypeReviewsMapEntity;
+import org.hidevelop.coffeecats.model.entity.id.TypeReviewsMapId;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TypeReviewsMapRepository extends JpaRepository<TypeReviewsMapEntity, TypeReviewsMapId> {
+}
