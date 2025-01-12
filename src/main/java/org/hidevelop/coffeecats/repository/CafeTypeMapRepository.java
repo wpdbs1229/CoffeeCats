83c5e5cfb34868f6c21b6719b607350f3b3add28
LeeJeYoun <dlwpdbs1229@naver.com>
2025-01-12 15:30:54 +0900

    refactor : "CafeTypeMapRepository의 pk값을 CafeTypeMapId로 변경" - CafeTypeMapId를 따로 long id로 조회보단, 복합키 cafeEntity와 cafeTypeEntity로 조회가 많이 일어나므로 해당 복합키를 pk값으로 지정함에 따라 CafeTypeMapId로 변경 Related to: #15

--- src/main/java/org/hidevelop/coffeecats/repository/CafeTypeMapRepository.java
+++ src/main/java/org/hidevelop/coffeecats/repository/CafeTypeMapRepository.java
@@ -1,10 +1,10 @@
 package org.hidevelop.coffeecats.repository;
 
 import org.hidevelop.coffeecats.model.entity.CafeTypeMapEntity;
+import org.hidevelop.coffeecats.model.entity.id.CafeTypeMapId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CafeTypeMapRepository extends JpaRepository<CafeTypeMapEntity, Long> {
-
+public interface CafeTypeMapRepository extends JpaRepository<CafeTypeMapEntity, CafeTypeMapId> {
 }
