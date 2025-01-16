ab297cdb52f033788c08b8b16b4a609ab7c0cfd3
LeeJeYoun <dlwpdbs1229@naver.com>
2025-01-16 19:50:28 +0900

    feat : "카페 수정 기능 구현" - CafeError에 DOES_NOT_EXIST_CAFE 추가( 찾으려는 카페가 없을 떄, 해당 오류를 내보내기 위함) Related to: #17

--- src/main/java/org/hidevelop/coffeecats/exception/error/impl/CafeError.java
+++ src/main/java/org/hidevelop/coffeecats/exception/error/impl/CafeError.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 public enum CafeError implements CustomError {
 
     ALREADY_REGISTERED(HttpStatus.BAD_REQUEST, "이미 등록된 카페입니다."),
+    DOES_NOT_EXIST_CAFE(HttpStatus.BAD_REQUEST, "등록되지 않은 카페입니다."),
     DOES_NOT_EXIST_CAFE_TYPE(HttpStatus.BAD_REQUEST, "존재하지 않은 카페타입 입니다.")
     ;
     private final HttpStatus httpStatus;
