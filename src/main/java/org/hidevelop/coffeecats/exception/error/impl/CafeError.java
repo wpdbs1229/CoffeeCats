9c9e722cc63c5c1f2107ebf72c888f1bd9c0eb25
LeeJeYoun <dlwpdbs1229@naver.com>
2025-01-17 18:19:13 +0900

    feat : "카페 수정 기능 구현" - 카페를 등록한 사람만 카페 설명을 수정할 수 있게 하기 위해 NOT_AUTHORIZED_TO_EDIT_CAFE 에러 추가 Related to: #17

--- src/main/java/org/hidevelop/coffeecats/exception/error/impl/CafeError.java
+++ src/main/java/org/hidevelop/coffeecats/exception/error/impl/CafeError.java
@@ -11,8 +11,10 @@ public enum CafeError implements CustomError {
 
     ALREADY_REGISTERED(HttpStatus.BAD_REQUEST, "이미 등록된 카페입니다."),
     DOES_NOT_EXIST_CAFE(HttpStatus.BAD_REQUEST, "등록되지 않은 카페입니다."),
-    DOES_NOT_EXIST_CAFE_TYPE(HttpStatus.BAD_REQUEST, "존재하지 않은 카페타입 입니다.")
+    DOES_NOT_EXIST_CAFE_TYPE(HttpStatus.BAD_REQUEST, "존재하지 않은 카페타입 입니다."),
+    NOT_AUTHORIZED_TO_EDIT_CAFE(HttpStatus.BAD_REQUEST, "등록한 사람에 한 해 카페 설명을 수정할 수 있습니다."),
     ;
+
     private final HttpStatus httpStatus;
     private final String message;
 }
