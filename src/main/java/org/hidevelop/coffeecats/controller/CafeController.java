f0ff8a7110aac51045c109017639e1b06b1578dc
LeeJeYoun <dlwpdbs1229@naver.com>
2025-01-18 19:50:13 +0900

    refactor : "카페 타입에 맞는 주변 카페 검색" - 각 cafe API 설명 주석 추가 - CommonResDto를 카페 등록 API 응답에 추가 Related to: #19

--- src/main/java/org/hidevelop/coffeecats/controller/CafeController.java
+++ src/main/java/org/hidevelop/coffeecats/controller/CafeController.java
@@ -22,17 +22,29 @@ public class CafeController {
     private final CafeService cafeService;
     private final GooglePlaceClient googlePlaceClient;
 
+    /***
+     * 카페 등록 API
+     * @param registerCafeReqDto 카페 정보 DTO
+     * @param request JWT 분해를 위해
+     * @return 카페 등록 성공 값
+     */
     @JwtAuth
     @PostMapping("/register")
-    public ResponseEntity<String> registerCafe(
+    public ResponseEntity<CommonResponseDto> registerCafe(
             @Valid @RequestBody RegisterCafeReqDto registerCafeReqDto,
             HttpServletRequest request) {
         // HttpServletRequest의 getAttribute() 메서드는 Object 타입을 반환하기 때문에 원시 타입으로 바로 사용할 수 없음
         Long memberId = (Long) request.getAttribute("memberId");
         cafeService.registerCafe(registerCafeReqDto, memberId);
-        return ResponseEntity.ok("카페가 등록되었습니다.");
+        return ResponseEntity.ok(new CommonResponseDto("카페 등록에 성공했습니다."));
     }
 
+    /***
+     * 등록할 카페를 주변에서 검색
+     * @param longitude 사용자의 경도
+     * @param latitude 사용자의 위도
+     * @return 주변 카페 목록
+     */
     @JwtAuth
     @GetMapping("/search-nearby/register-cafe")
     public ResponseEntity<GooglePlaceResponseDto> searchRegisterCafe(@RequestParam double longitude,
@@ -52,6 +64,13 @@ public class CafeController {
         return ResponseEntity.ok(googlePlaceResponseDtos);
     }
 
+    /***
+     * 등록할 카페를 주변에서 텍스트로 검색
+     * @param textQuery 검색어
+     * @param longitude 사용자 경도
+     * @param latitude 사용자 위도
+     * @return 검색한 언어와 관련된 주변 카페 목록
+     */
     @JwtAuth
     @GetMapping("/search-text/register-cafe")
     public ResponseEntity<?> searchRegisterCafe(@RequestParam String textQuery,
@@ -75,6 +94,13 @@ public class CafeController {
         return ResponseEntity.ok(googlePlaceResponseDto);
     }
 
+    /***
+     * 카페 타입 수정 API
+     * @param cafeId 수정할려고 하는 카페 ID
+     * @param cafeTypeReviewsUpdateReqDto 수정할려는 cafe Type List
+     * @param request JWT
+     * @return 카페 수정 성공 메시지
+     */
     @JwtAuth
     @PutMapping("/{cafe-id}/cafe-type")
     public ResponseEntity<CommonResponseDto> updateCafeType(
@@ -88,6 +114,13 @@ public class CafeController {
         return ResponseEntity.ok(new CommonResponseDto("카페 수정을 성공하였습니다."));
     }
 
+    /***
+     * 카페 설명 수정 API
+     * @param cafeId 수정할려고 하는 카페 ID
+     * @param cafeDescriptionUpdateReqDto 수정할 카페 설명
+     * @param request JWT
+     * @return 카페 수정 메시지
+     */
     @JwtAuth
     @PutMapping("/{cafe-id}/cafe-description")
     public ResponseEntity<CommonResponseDto> updateCafeDescription(
